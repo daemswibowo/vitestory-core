e241998bed2dcd6e4e403d31909c9e684bf57d1f
Dimas Wibowo <daemswibowo@gmail.com>
2024-08-01 07:23:48 +0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {
+  buildTemplateView,
+  mustacheUtils,
+  renderTemplate,
+} from "./src/utils/template";
+
+export const utils = {
+  template: {
+    buildTemplateView,
+    mustacheUtils,
+    renderTemplate,
+  },
+};
